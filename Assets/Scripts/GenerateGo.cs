bcd1bed200d9f10035bcf87d89eff9b2fdbadc5d
LewOF04 <Lewislof123@gmail.com>
2025-11-04 18:23:21 +0000

    Fixed input parsing out of range issues

--- Assets/Scripts/GenerateGo.cs
+++ Assets/Scripts/GenerateGo.cs
@@ -11,18 +11,29 @@ public class GenerateGo : MonoBehaviour
     {
         string text = inputField.text;
         Debug.Log(text);
-        int input;
 
-        bool success = Int32.TryParse(text, out input);
+        bool success = ulong.TryParse(text, out ulong input);
+
+        int seed;
+        if (input > (ulong)Int32.MaxValue)
+        {
+            seed = (int)input % Int32.MaxValue;
+        }
+        else
+        {
+            seed = (int)input;
+        }
+       
+        Debug.Log(input.ToString());
 
         if (!success)
         {
-            Debug.Log("Not success");
+            Debug.Log("Your seed value must be an integer numerical value, less than 18,446,744,073,709,551,615.");
             //TODO: return error that it wasn't a valid integer
         }
         else
         {
-          mainMenu.GenerateGame(input);  
+          mainMenu.GenerateGame(seed);  
         }
         
     }
