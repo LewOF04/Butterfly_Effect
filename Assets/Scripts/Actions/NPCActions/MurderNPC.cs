160a119b1ec13c3a2c51a284cb8fcdb953dd5722
LewOF04 <Lewislof123@gmail.com>
2026-04-07 22:42:40 +0100

    Murder NPC Action

---
+++ Assets/Scripts/Actions/NPCActions/MurderNPC.cs
@@ -0,0 +1,341 @@
+using UnityEngine;
+using UnityEngine.Scripting;
+using System.Collections.Generic;
+using NoTarget = System.ValueTuple;
+
+[Preserve]
+public class MurderNPC : NPCAction
+{
+    public MurderNPC() : base('N'){}
+
+    public override char actionType => 'N';
+    public override string name => "Murder Person";
+    public override string baseDescription => "Sometimes the most extreme action is necessary.";
+
+    protected override float baseTime => 3f;
+    protected override float baseEnergy => 60f;
+    protected override float complexity => 5f;
+    protected override float baseUtility => 40f;
+    public override bool interrupts => true;
+
+    protected override List<int> utilityPosTraits => new List<int>{2}; 
+    protected override List<int> utilityNegTraits => new List<int>{1,5};
+    protected override List<int> successPosTraits => new List<int>{5}; 
+    protected override List<int> successNegTraits => new List<int>{};
+
+    //compute the empirical utility of the action
+    protected override float computeUtility(IAgent performer, IAgent target)
+    {
+        if(actUtility != -1) return actUtility;
+
+        if(timeToComplete == -1) getTimeToComplete(performer, target);
+        if(energyToComplete == -1) getEnergyToComplete(performer, target);
+        if(actSuccess == -1) computeSuccess(performer, target);
+
+        List<float> effectors = new List<float>();
+        List<float> weights = new List<float>();
+
+        //IAgent stat/attribute effectors
+        effectors.Add(ActionMaths.calcMultiplier(target.stats.condition, 0f, 100f, 2f, 0.25f)); weights.Add(0.8f);
+        effectors.Add(ActionMaths.calcMultiplier(target.attributes.strength, 0f, 100f, 2f, 0.25f)); weights.Add(0.8f);
+
+        effectors.Add(ActionMaths.calcMultiplier(performer.attributes.strength, 0f, 100f, 0.25f, 2f)); weights.Add(0.8f);
+        effectors.Add(ActionMaths.calcMultiplier(performer.stats.happiness, 0f, 100f, 2f, 0.1f)); weights.Add(0.6f);
+        effectors.Add(ActionMaths.calcMultiplier(performer.attributes.morality, 0f, 100f, 2f, 0.1f)); weights.Add(0.7f);
+
+        effectors.Add(ActionMaths.calcMultiplier(performer.attributes.constitution, 0f, 100f, 0.25f, 2f)); weights.Add(0.4f);
+        effectors.Add(ActionMaths.calcExpMultiplier(performer.attributes.morality, 100f, 0f, 3f, 0.1f, 5f)); weights.Add(0.9f);
+        effectors.Add(ActionMaths.calcMultiplier(performer.stats.nutrition, 0f, 100f, 0.25f, 2f)); weights.Add(0.2f);
+        effectors.Add(ActionMaths.calcMultiplier(performer.stats.condition, 0f, 100f, 2f, 0.25f)); weights.Add(0.3f);
+
+        //energy and time effectors
+        effectors.Add(ActionMaths.scarcityMultiplier(performer.stats.energy - energyToComplete, 0f, 100f, 0.1f, 2f)); weights.Add(1.5f);
+        effectors.Add(ActionMaths.calcMultiplier(timeToComplete, 0f, 24f, 2f, 0.25f)); weights.Add(0.5f);
+        effectors.Add(ActionMaths.calcMultiplier(actSuccess, 0f, 100f, 0.25f, 2f)); weights.Add(Mathf.InverseLerp(0f, 100f, performer.attributes.wisdom));
+
+        actUtility = ActionMaths.ApplyWeightedMultipliers(baseUtility, effectors, weights);
+        return actUtility;
+    }
+
+    //compute the performers perceived utility of the action
+    protected override float estimateUtility(IAgent performer, IAgent target)
+    {
+        if(estUtility != -1) return estUtility;
+        
+        if(actUtility == -1) actUtility = computeUtility(performer, default);
+
+        estUtility = ActionMaths.addTraitWeights(performer, actUtility, utilityPosTraits, true);
+        estUtility = ActionMaths.addTraitWeights(performer, estUtility, utilityNegTraits, false); 
+
+        List<float> effectors = new List<float>();
+        List<float> weights = new List<float>();
+
+        Relationship thisRel = dataController.RelationshipStorage[new RelationshipKey(performer.id, target.id)];
+        effectors.Add(ActionMaths.calcMultiplier(thisRel.value, 0f, 100f, 2f, 0.25f)); weights.Add(Mathf.Clamp(Mathf.InverseLerp(0f, 100f, performer.attributes.wisdom), 0.2f, 0.8f));
+        effectors.Add(ActionMaths.calcMultiplier(target.stats.wealth, 0f, 100f, 0.25f, 2f)); weights.Add(Mathf.InverseLerp(0f, 100f, performer.attributes.perception));
+        effectors.Add(ActionMaths.calcMultiplier(performer.stats.happiness, 0f, 100f, 0.25f, 2f)); weights.Add(1 - Mathf.InverseLerp(0f, 100f, performer.attributes.fortitude));
+
+        estUtility = ActionMaths.ApplyWeightedMultipliers(estUtility, effectors, weights);    
+
+        estUtility = ActionMaths.rationalityNoise(estUtility, performer.attributes.rationality);
+        return estUtility;
+    }
+
+    protected override void innerPerformAction(float percentComplete)
+    {
+        if(!dataController.TryGetAgent(currentActor, out var performer)) return;
+        if(!dataController.TryGetAgent(receiver, out var target)) return;
+
+        float percentMulti = percentComplete / 100;
+        string description = performer.fullName + " spent " + (percentMulti*timeToComplete).ToString("0.00") + " hours attempting to kill " + target.fullName + ".";
+        ActionResult successInfo = ActionMaths.calcActionSuccess(actSuccess, percentComplete);
+
+        //add description and result levels based on success
+        float killMultiplier;
+        if(successInfo.success == true) {
+            description += "They succeeded in killing them ";
+
+            if(successInfo.quality < 0.25f) {description += "but it was messy and " + target.fullName + " resisted fiercely."; killMultiplier = 0.33f;}
+            else if(successInfo.quality < 0.5f) {description += "though it took longer than expected and was not clean."; killMultiplier = 0.66f;}
+            else if(successInfo.quality < 0.75f) {description += "with a decisive and controlled strike."; killMultiplier = 1f;}
+            else {description += "swiftly and without leaving much trace."; killMultiplier = 1.33f;}
+        }
+        else {
+            description += "Their attempt to kill was not successful ";
+
+            if(successInfo.quality < 0.25f) {description += "they were spotted too early and couldn’t get close."; killMultiplier = 0f;}
+            else if(successInfo.quality < 0.5f) {description += target.fullName + " fought them off and forced them to retreat."; killMultiplier = -0.1f;}
+            else if(successInfo.quality < 0.75f) {description += "they made too much noise and the attempt failed under pressure."; killMultiplier = -0.2f;}
+            else {description += "and they were left injured after the failed attempt."; killMultiplier = -0.3f;}
+        }
+
+        if(percentComplete != 100f) description += " The attempt concluded " + percentComplete.ToString("0.00") + "% of the way through.";
+                
+        float actionTime = dataController.World.gameTime;
+
+        //perform changes to stats/attributes
+        description += "\n";
+
+        //energy changes
+        float energyMinus = energyToComplete * percentMulti;
+        performer.stats.energy = Mathf.Max(0f, performer.stats.energy - energyMinus);
+        description += "They spent "+energyMinus.ToString("0.00")+" energy, ";
+
+        //time changes
+        float timeMinus = timeToComplete * percentMulti;
+        description += timeMinus.ToString("0.00")+" hours";
+
+        //health changes
+        float healthDiff = Mathf.Abs(killMultiplier) * 15f;
+        if(killMultiplier < 0)
+        {
+            float condChange = Mathf.Pow(healthDiff, 1 - Mathf.InverseLerp(0f, 100f, performer.attributes.constitution));
+            performer.stats.condition = Mathf.Max(0f, performer.stats.condition - condChange);
+            description += " The performer was damaged by "+condChange.ToString("0.00")+" health.";
+        } else if (killMultiplier > 0)
+        {
+            if(killMultiplier >= 1)
+            {
+                target.stats.condition = -100f;
+                description += "The target was killed by the attacker.";
+            }
+            else
+            {
+                float condChange = Mathf.Pow(healthDiff, 1 - Mathf.InverseLerp(0f, 100f, target.attributes.constitution));
+                target.stats.condition = Mathf.Max(0f, target.stats.condition - condChange);
+                description += " The target was damaged by "+condChange.ToString("0.00")+" health.";
+            }
+        }
+        else
+        {
+            description += " No damage was taken by any involved."; 
+        }
+
+        //relationship changes
+        float relChange = 60f * Mathf.Pow(Mathf.Abs(killMultiplier), 1 - Mathf.InverseLerp(0f, 100f, target.attributes.perception));
+        Relationship rel = dataController.RelationshipStorage[new RelationshipKey(target.id, performer.id)];
+        rel.value = Mathf.Max(0f, rel.value - relChange);
+
+        List<Relationship> targetRelationships = dataController.RelationshipPerNPCStorage[target.id];
+        for(int i = 0; i < targetRelationships.Count - 1; i++)
+        {
+            Relationship tRel = targetRelationships[i];
+            int otherID;
+            if(tRel.key.npcA == target.id) otherID = tRel.key.npcB;
+            else otherID = tRel.key.npcA;
+
+            Relationship perfRel = dataController.RelationshipStorage[new RelationshipKey(performer.id, otherID)];
+            if(tRel.value > 50)
+            {
+                perfRel.value = Mathf.Clamp(perfRel.value + Mathf.Lerp(-20f, 0f, Mathf.InverseLerp(50f, 100f, tRel.value)), 0f, 100f);
+            } else if(tRel.value < 20)
+            {
+                perfRel.value = Mathf.Clamp(perfRel.value + Mathf.Lerp(0f, 20f, Mathf.InverseLerp(0f, 20f, tRel.value)), 0f, 100f);
+            }
+        }
+        description += " Their relationship was impacted to all that knew who they killed.";
+
+        //happiness changes
+        if (performer.traits.Contains(5)) //if they are a serial killer
+        {
+            performer.stats.happiness = Mathf.Min(100f, performer.stats.happiness + 10f);
+        }
+
+        //morality changes
+        if(killMultiplier >= 0) performer.attributes.morality = Mathf.Max(0f, performer.attributes.morality - (1f + 9f * killMultiplier)); 
+        else performer.attributes.morality = Mathf.Min(100f, performer.attributes.morality + 1f);
+
+        bool wasPosPerf = false;
+        bool wasPosRec = false;
+        if(killMultiplier > 0) {wasPosPerf = true; wasPosRec = false;}
+        else {wasPosPerf = false; wasPosRec = false;}
+
+        float severity = 50f * Mathf.Abs(killMultiplier);
+        HistoryManager.Instance.AddNPCMemory(name, description, severity, actionTime, performer.id, target.id, wasPosPerf, wasPosRec);
+    }
+
+    //computer the likelihood this action will be a success
+    protected override float computeSuccess(IAgent performer, IAgent target)
+    {
+        if(actSuccess != -1) return actSuccess;
+
+        List<float> effectors = new List<float>();
+        List<float> weights = new List<float>();
+
+        //==================Relationship Outline==================
+        Relationship thisRel = dataController.RelationshipStorage[new RelationshipKey(performer.id, target.id)];
+        effectors.Add(ActionMaths.calcMultiplier(thisRel.value, 0f, 100f, 0.25f, 2f)); weights.Add(0.5f); //better relationship makes it more likely that they'll succeed
+
+        //==================Performer Stats==================
+        effectors.Add(ActionMaths.calcExpMultiplier(performer.stats.condition, 100f, 0f, 2f, 0.25f, 5f)); weights.Add(0.8f); 
+        effectors.Add(ActionMaths.calcMultiplier(performer.stats.energy, 0f, 100f, 0.25f, 2f)); weights.Add(0.3f);
+
+        //==================Performer Attributes==================
+        effectors.Add(ActionMaths.calcMultiplier(performer.attributes.fortitude, 0f, 100f, 0.25f, 2f)); weights.Add(0.2f);
+        effectors.Add(ActionMaths.calcMultiplier(performer.attributes.perception, 0f, 100f, 0.25f, 2f)); weights.Add(0.7f);
+        effectors.Add(ActionMaths.calcMultiplier(performer.attributes.dexterity, 0f, 100f, 0.25f, 2f)); weights.Add(0.6f);
+        effectors.Add(ActionMaths.calcMultiplier(performer.attributes.strength, 0f, 100f, 0.25f, 2f)); weights.Add(0.9f);
+
+        //==================Target Stats==================
+        effectors.Add(ActionMaths.calcMultiplier(target.stats.condition, 0f, 100f, 2f, 0.5f)); weights.Add(0.8f);
+        effectors.Add(ActionMaths.calcMultiplier(target.stats.energy, 0f, 100f, 0.25f, 2f)); weights.Add(0.3f);
+
+        //==================Target Attributes==================
+        effectors.Add(ActionMaths.calcMultiplier(target.attributes.charisma, 0f, 100f, 1.25f, 0.75f)); weights.Add(0.2f);
+        effectors.Add(ActionMaths.calcMultiplier(target.attributes.perception, 0f, 100f, 2f, 0.25f)); weights.Add(0.7f);
+        effectors.Add(ActionMaths.calcMultiplier(target.attributes.dexterity, 0f, 100f, 2f, 0.25f)); weights.Add(0.6f);
+        effectors.Add(ActionMaths.calcMultiplier(target.attributes.strength, 0f, 100f, 2f, 0.25f)); weights.Add(0.9f);
+
+        float weightedSucc = ActionMaths.ApplyWeightedMultipliers(50f, effectors, weights);
+
+        //add 5% for each time this IAgent has performed the action in the past
+        List<NPCEvent> events = dataController.eventsPerNPCStorage[currentActor];
+        foreach(NPCEvent thisEvent in events)
+        {
+            if(thisEvent.actionName == name && thisEvent.performer == currentActor)
+            {
+               weightedSucc = Mathf.Min(100f, weightedSucc + weightedSucc * 0.05f); 
+            }
+        }  
+
+        actSuccess = weightedSucc;
+        return actSuccess;
+    }
+
+    //compute the estimated chance this action will be a succss from the performers perspective
+    protected override float estimateSuccess(IAgent performer, IAgent target)
+    {
+        if(estSuccess != -1) return estSuccess;
+        
+        float baseSuccess;
+        if(actSuccess == -1) baseSuccess = computeSuccess(performer, default);
+        else baseSuccess = actSuccess;
+
+        estSuccess = ActionMaths.rationalityNoise(baseSuccess, performer.attributes.rationality);
+        return estSuccess;
+    }
+
+    //calculate how much time it would take for the IAgent to complete this action
+    protected override float getTimeToComplete(IAgent performer, IAgent target)
+    {
+        if(timeToComplete != -1) return timeToComplete;
+
+        List<float> effectors = new List<float>();
+        List<float> weights = new List<float>();
+
+        //==================Performer Stats==================
+        effectors.Add(ActionMaths.calcExpMultiplier(performer.stats.condition, 100f, 0f, 0.25f, 2f, 5f)); weights.Add(0.8f); 
+        effectors.Add(ActionMaths.calcMultiplier(performer.stats.energy, 0f, 100f, 2f, 0.25f)); weights.Add(0.3f);
+
+        //==================Performer Attributes==================
+        effectors.Add(ActionMaths.calcMultiplier(performer.attributes.fortitude, 0f, 100f, 2f, 0.25f)); weights.Add(0.5f);
+        effectors.Add(ActionMaths.calcMultiplier(performer.attributes.perception, 0f, 100f, 2f, 0.25f)); weights.Add(0.7f);
+        effectors.Add(ActionMaths.calcMultiplier(performer.attributes.dexterity, 0f, 100f, 2f, 0.25f)); weights.Add(0.6f);
+        effectors.Add(ActionMaths.calcMultiplier(performer.attributes.strength, 0f, 100f, 2f, 0.25f)); weights.Add(0.9f);
+
+        //==================Target Stats==================
+        effectors.Add(ActionMaths.calcMultiplier(target.stats.condition, 0f, 100f, 2f, 0.5f)); weights.Add(0.8f);
+        effectors.Add(ActionMaths.calcMultiplier(target.stats.energy, 0f, 100f, 0.25f, 2f)); weights.Add(0.3f);
+
+        //==================Target Attributes==================
+        effectors.Add(ActionMaths.calcMultiplier(target.attributes.charisma, 0f, 100f, 0.75f, 1.25f)); weights.Add(0.2f);
+        effectors.Add(ActionMaths.calcMultiplier(target.attributes.perception, 0f, 100f, 0.25f, 3f)); weights.Add(0.7f);
+        effectors.Add(ActionMaths.calcMultiplier(target.attributes.dexterity, 0f, 100f, 0.25f, 3f)); weights.Add(0.6f);
+        effectors.Add(ActionMaths.calcMultiplier(target.attributes.strength, 0f, 100f, 0.25f, 2f)); weights.Add(0.9f);
+
+        float weightedTime = ActionMaths.ApplyWeightedMultipliers(baseTime, effectors, weights);
+        weightedTime = ActionMaths.addChaos(weightedTime, dataController.World.chaosModifier);
+
+        timeToComplete = weightedTime;
+        return timeToComplete;
+    }
+
+    //calculate how much energy it would take the IAgent to compelete this action
+    protected override float getEnergyToComplete(IAgent performer, IAgent target)
+    {
+        if(energyToComplete != -1) return energyToComplete;
+
+        List<float> effectors = new List<float>();
+        List<float> weights = new List<float>();
+
+        //==================Performer Stats==================
+        effectors.Add(ActionMaths.calcExpMultiplier(performer.stats.condition, 100f, 0f, 0.25f, 2f, 5f)); weights.Add(0.8f); 
+        effectors.Add(ActionMaths.calcMultiplier(performer.stats.energy, 0f, 100f, 2f, 0.25f)); weights.Add(0.3f);
+
+        //==================Performer Attributes==================
+        effectors.Add(ActionMaths.calcMultiplier(performer.attributes.fortitude, 0f, 100f, 2f, 0.25f)); weights.Add(0.5f);
+        effectors.Add(ActionMaths.calcMultiplier(performer.attributes.perception, 0f, 100f, 2f, 0.25f)); weights.Add(0.7f);
+        effectors.Add(ActionMaths.calcMultiplier(performer.attributes.dexterity, 0f, 100f, 2f, 0.25f)); weights.Add(0.6f);
+        effectors.Add(ActionMaths.calcMultiplier(performer.attributes.strength, 0f, 100f, 2f, 0.25f)); weights.Add(0.9f);
+
+        //==================Target Stats==================
+        effectors.Add(ActionMaths.calcMultiplier(target.stats.condition, 0f, 100f, 2f, 0.5f)); weights.Add(0.8f);
+        effectors.Add(ActionMaths.calcMultiplier(target.stats.energy, 0f, 100f, 0.25f, 2f)); weights.Add(0.3f);
+
+        //==================Target Attributes==================
+        effectors.Add(ActionMaths.calcMultiplier(target.attributes.charisma, 0f, 100f, 0.75f, 1.25f)); weights.Add(0.2f);
+        effectors.Add(ActionMaths.calcMultiplier(target.attributes.perception, 0f, 100f, 0.25f, 3f)); weights.Add(0.7f);
+        effectors.Add(ActionMaths.calcMultiplier(target.attributes.dexterity, 0f, 100f, 0.25f, 3f)); weights.Add(0.6f);
+        effectors.Add(ActionMaths.calcMultiplier(target.attributes.strength, 0f, 100f, 0.25f, 2f)); weights.Add(0.9f);
+
+        float weightedEnergy = ActionMaths.ApplyWeightedMultipliers(baseEnergy, effectors, weights);
+        weightedEnergy = ActionMaths.addChaos(weightedEnergy, dataController.World.chaosModifier);
+
+        energyToComplete = weightedEnergy;
+        return energyToComplete;
+    }
+
+    protected override bool isKnown(IAgent performer)
+    {
+        float thisComplexity = complexity;
+
+        //either the action is known because they're smart enough
+        if (complexity <= performer.attributes.intelligence) return true;
+
+        //or they have memory of a similar event
+        List<NPCEvent> npcEvents = dataController.eventsPerNPCStorage[performer.id];
+        foreach(NPCEvent thisEvent in npcEvents)  if(thisEvent.actionName == name) return true;
+
+        return false;
+    } 
+}
